48ed1e8c27367bfc49d3e9c9ec360e17df0dd673
Koda <12773720+0x0Koda@users.noreply.github.com>
2023-08-30 19:50:05 +1000

    updated

--- index.ts
+++ index.ts
@@ -3,69 +3,72 @@ import { ethers } from "ethers";
 import dotenv from "dotenv";
 dotenv.config();
 
-
 const privateKey = process.env.PK!;
 if (!privateKey)
-    throw new Error("No private key provided, pls include in .env file");
+  throw new Error("No private key provided, pls include in .env file");
 
 (async () => {
-    // instantiate the SDK
-    //const baseUrl =
-    //  "https://squid-api-git-feat-cosmos-maintestnet-0xsquid.vercel.app";
-    const baseUrl = "http://localhost:3000";
-    const squid = new Squid({
-        baseUrl: baseUrl,
-    });
-    // init the SDK
-    await squid.init();
-    console.log("Squid inited");
-    const fromChainId = 43114;
-    const toChainId = 1284;
-    const provider = ethers.getDefaultProvider(
-        squid.chains.find((c) => c.chainId === fromChainId)!.rpc
-    );
-    const signer = new ethers.Wallet(privateKey, provider);
-    const params = {
-        fromChain: fromChainId,
-        fromToken: squid.tokens.find(
-            (t) =>
-                t.symbol.toLocaleLowerCase() === "AVAX" && t.chainId === fromChainId
-        )!.address,
-        fromAmount: ethers.utils.parseUnits("0.5", "6").toString(),
-        toChain: toChainId,
-        toToken: squid.tokens.find(
-            (t) =>
-                t.symbol.toLocaleLowerCase() === "axlusdc" && t.chainId === toChainId
-        )!.address,
-        toAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
-        slippage: 3.0,
-        enableForecall: false,
-        quoteOnly: false,
-        /* collectFees: {
+  // instantiate the SDK
+  //const baseUrl =
+  //  "https://squid-api-git-feat-cosmos-maintestnet-0xsquid.vercel.app";
+  const baseUrl = "http://localhost:3000";
+  const squid = new Squid();
+
+  squid.setConfig({
+    baseUrl: baseUrl, // for mainnet use "https://api.0xsquid.com"
+    integratorId: "trustwallet-api",
+  });
+
+  // init the SDK
+  await squid.init();
+  console.log("Squid inited");
+  const fromChainId = 43113;
+  const toChainId = 5;
+  const provider = ethers.getDefaultProvider(
+    squid.chains.find((c) => c.chainId === fromChainId)!.rpc
+  );
+  const signer = new ethers.Wallet(privateKey, provider);
+  const params = {
+    fromChain: fromChainId,
+    fromToken: squid.tokens.find(
+      (t) =>
+        t.symbol.toLocaleLowerCase() === "avax" && t.chainId === fromChainId
+    )!.address,
+    fromAmount: ethers.utils.parseUnits("0.5", "18").toString(),
+    toChain: toChainId,
+    toToken: squid.tokens.find(
+      (t) => t.symbol.toLocaleLowerCase() === "ausdc" && t.chainId === toChainId
+    )!.address,
+    fromAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
+    toAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
+    slippage: 3.0,
+    enableForecall: false,
+    quoteOnly: false,
+    /* collectFees: {
             integratorAddress: "0xb13CD07B22BC5A69F8500a1Cb3A1b65618d50B22",
             fee: 90
         } */
-    };
+  };
 
-    console.log("route params", params);
-    const { route } = await squid.getRoute(params);
-    console.log(route);
-    const overrides = {
-        maxPriorityFeePerGas: ethers.utils.parseUnits("5", "gwei"),
-        maxFeePerGas: ethers.utils.parseUnits("200", "gwei"),
-    };
+  console.log("route params", params);
+  const { route } = await squid.getRoute(params);
+  console.log(route);
+  /* const overrides = {
+    maxPriorityFeePerGas: ethers.utils.parseUnits("5", "gwei"),
+    maxFeePerGas: ethers.utils.parseUnits("200", "gwei"),
+  }; */
 
-    /* const tx = (await squid.executeRoute({
+  /* const tx = (await squid.executeRoute({
         signer,
         route,
         //overrides: overrides
     })) as ethers.providers.TransactionResponse;
     const txReceipt = await tx.wait();
     const txHash = txReceipt.transactionHash; */
-    //const txHash =
-    //  "0xa3f446fe845f528fecc62ec6e860d02a9b9a515087d19b84632bcccc6e978bdd";
+  //const txHash =
+  //  "0xa3f446fe845f528fecc62ec6e860d02a9b9a515087d19b84632bcccc6e978bdd";
 
-    /*
+  /*
     await sleep(5); //wait for axelar to index
     let statusResult = false;
     while (!statusResult) {
@@ -92,9 +95,9 @@ if (!privateKey)
             //console.log(error);
         }
     } */
-    //console.log(`https://testnet.axelarscan.io/gmp/${txReceipt.transactionHash}`);
+  //console.log(`https://testnet.axelarscan.io/gmp/${txReceipt.transactionHash}`);
 })();
 
 const sleep = async (time: number) => {
-    new Promise((r) => setTimeout(r, time * 1000));
+  new Promise((r) => setTimeout(r, time * 1000));
 };
